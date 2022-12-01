3ae63cae04960e5fdfd7a0a0dc56fad12ba28f59
Daniel Ekström <mr.oakstream@gmail.com>
2022-12-01 13:25:29 +0100

    Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,36 @@
-pub fn part1(_input: String) {
-	println!("Part 1 test");
+fn get_sorted_calories(input: String) -> Vec<usize> {
+	let lines: Vec<String> = input
+		.lines()
+		.map(|line| line.parse::<String>().unwrap())
+		.collect();
+
+	let mut sum: usize = 0;
+	let mut calories: Vec<usize> = vec![];
+
+	for line in lines.iter() {
+		if line == "" {
+			calories.push(sum);
+			sum = 0;
+		} else {
+			sum += line.parse::<usize>().unwrap();
+		}
+	}
+
+	calories.push(sum);
+	calories.sort();
+
+	calories
 }
 
-pub fn part2(_input: String) {
-	println!("Part 2 test");
+pub fn part1(input: String) {
+	let calories: Vec<usize> = get_sorted_calories(input);	
+
+	println!("{}", calories.last().unwrap());
+}
+
+pub fn part2(input: String) {
+	let calories: Vec<usize> = get_sorted_calories(input);	
+	let result: usize = calories.iter().rev().take(3).sum();
+
+	println!("{}", result);
 }

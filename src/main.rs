d0525f61fb658558841b232ecefa026df1dd29ad
Daniel Ekström <mr.oakstream@gmail.com>
2022-12-01 15:09:51 +0100

    Added timing of parts

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,37 @@
 
 use std::env;
 use std::fs;
+use std::time::{Duration, Instant};
 
 use advent_of_code::{get_day, noop};
+
+fn fmt_time(ms: f64) -> String {
+    if ms <= 1.0 {
+        let micro_sec = ms * 1000.0;
+        return String::from(format!("{}µs", micro_sec.round()));
+    }
+
+    if ms < 1000.0 {
+        let whole_ms = ms.floor();
+        let rem_ms = ms - whole_ms;
+        return String::from(format!("{}ms ", whole_ms) + &fmt_time(rem_ms));
+    }
+
+    let sec: f64 = ms / 1000.0;
+    if sec < 60.0 {
+        let whole_sec = sec.floor();
+        let rem_ms = ms - whole_sec * 1000.0;
+
+        return format!("{}s ", whole_sec) + &fmt_time(rem_ms);
+    }
+
+    let min: f64 = sec / 60.0;
+    return format!("{}m ", min.floor()) + &fmt_time((sec % 60.0) * 1000.0);
+}
+
+fn fmt_duration(dur: Duration) -> String {
+    return fmt_time(dur.as_secs_f64() * 1000.0);
+}
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
@@ -36,11 +65,17 @@ fn main() {
 	let to_run = get_day(day_num);
 
 	if to_run.0 != noop {
+		let start = Instant::now();
 		to_run.0(input.clone());
+		let duration = start.elapsed();
+		println!("Duration: {}", fmt_duration(duration));
 	}
 
 	if to_run.1 != noop {
+		let start = Instant::now();
 		to_run.1(input.clone());
+		let duration = start.elapsed();
+		println!("Duration: {}", fmt_duration(duration));
 	}
 }
 

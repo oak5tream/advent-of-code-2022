bcd96f22f283e9a8c05631a856d86fe2a1fb405e
Daniel Ekström <mr.oakstream@gmail.com>
2022-12-01 10:56:34 +0100

    Added last years framework

---
+++ src/day01.rs
@@ -0,0 +1,7 @@
+pub fn part1(_input: String) {
+	println!("Part 1 test");
+}
+
+pub fn part2(_input: String) {
+	println!("Part 2 test");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+// Template inspiration: https://github.com/nickyvanurk/advent-of-code-rust-template
+
+use std::env;
+use std::fs;
+
+use advent_of_code::{get_day, noop};
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+		println!("Expected a day and optional test");
+        return;
+    }
+
+	let mut day = args[1].clone();
+	let mut test = "".to_string();
+
+    if args.len() == 3 {
+		test.push_str(&format!("_{}", args[2]));
+	}
+
+	day = day.trim().to_string();
+	let day_num: u32 = match day.parse() {
+		Ok(num) => num,
+		Err(_) => {
+			println!("Invalid day number: {}", day);
+			return;
+		}
+	};
+
+	let cwd = env::current_dir().unwrap();
+	let filename = cwd.join("data").join(format!("day{:02}{}.txt", day_num, test));
+	println!("Reading {}", filename.display());
+	let input = fs::read_to_string(filename).expect("Error while reading");
+	let to_run = get_day(day_num);
+
+	if to_run.0 != noop {
+		to_run.0(input.clone());
+	}
+
+	if to_run.1 != noop {
+		to_run.1(input.clone());
+	}
 }
+

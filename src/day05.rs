f8c5a6ceedfd5da2ed5b68bbad26e4ee451ba1e7
Daniel Ekström <mr.oakstream@gmail.com>
2022-12-05 22:02:38 +0100

    Cleanup of day 5

--- src/day05.rs
+++ src/day05.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 
-pub fn part1(input: String) {
+fn get_solution(input: String, multiple_crates: bool) -> String
+{
 	let mut parsing_crates: bool = true;
 	let mut crates: Vec<Vec<char>> = vec![];
 
@@ -27,9 +28,23 @@ pub fn part1(input: String) {
 				let from: usize = &captures[2].parse::<usize>().unwrap() - 1;
 				let to: usize = &captures[3].parse::<usize>().unwrap() - 1;
 
-				for _ in 0 .. number {
-					let id = crates[from].pop().unwrap();
-					crates[to].push(id);
+				if multiple_crates {
+					let mut temp_crates: Vec<char> = vec![];
+
+					for _ in 0 .. number {
+						let id = crates[from].pop().unwrap();
+						temp_crates.push(id);
+					}
+
+					for _ in 0 .. number {
+						let id = temp_crates.pop().unwrap();
+						crates[to].push(id);
+					}
+				} else {
+					for _ in 0 .. number {
+						let id = crates[from].pop().unwrap();
+						crates[to].push(id);
+					}
 				}
 			}
 		}
@@ -43,61 +58,19 @@ pub fn part1(input: String) {
 		}
 	}
 
+	let mut result = "".to_string();
+
 	for i in 0 .. crates.len() {
-		println!("{:?}", crates[i].last().unwrap());
+		result.push_str(&format!("{}", crates[i].last().unwrap()));
 	}
+
+	result
+}
+
+pub fn part1(input: String) {
+	println!("{}", get_solution(input, false));
 }
 
 pub fn part2(input: String) {
-	let mut parsing_crates: bool = true;
-	let mut crates: Vec<Vec<char>> = vec![];
-
-	for line in input.lines() {
-		if parsing_crates {
-			for i in 0 .. line.len() {
-				if i > 0 && (i - 1) % 4 == 0 {
-					let id = line.chars().nth(i).unwrap();
-					let index = (i - 1) / 4;
-
-					if index + 1 > crates.len() {
-						crates.push(vec![]);
-					}
-
-					if id.is_ascii_uppercase() {
-						crates[index].push(id);
-					}
-				}
-			}
-		} else {
-			let re = Regex::new(r"move ([0-9]+) from ([0-9]) to ([0-9])").unwrap();
-			for captures in re.captures_iter(line) {
-				let number: usize = captures[1].parse::<usize>().unwrap();
-				let from: usize = &captures[2].parse::<usize>().unwrap() - 1;
-				let to: usize = &captures[3].parse::<usize>().unwrap() - 1;
-
-				let mut temp_crates: Vec<char> = vec![];
-				for _ in 0 .. number {
-					let id = crates[from].pop().unwrap();
-					temp_crates.push(id);
-				}
-
-				for _ in 0 .. number {
-					let id = temp_crates.pop().unwrap();
-					crates[to].push(id);
-				}
-			}
-		}
-
-		if line == "" {
-			parsing_crates = false;
-
-			for i in 0 .. crates.len() {
-				crates[i].reverse();
-			}
-		}
-	}
-
-	for i in 0 .. crates.len() {
-		println!("{:?}", crates[i].last().unwrap());
-	}
+	println!("{}", get_solution(input, true));
 }
